1b3f78672adab7a6954ae544d904e88477b249f1
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2023-03-10 12:10:08 -0300

    Requisitos 24, 25 e 26

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,24 +1,134 @@
-// import React, { useState } from 'react';
-// import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
-// function RecipeDetails() {
-//   const [details, setDetails] = useState([]);
-//   const location = useLocation();
-//   const { pathname } = location;
+function RecipeDetails() {
+  const [details, setDetails] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  const [youtubeVideo, setYoutubeVideo] = useState('');
+  const [categoryOrAlcoholic, setCategoryOrAlcoholic] = useState('');
+  const [recomendation, setRecomendation] = useState([]);
+  const location = useLocation();
+  const { pathname } = location;
 
-//   const id = pathname.split('/')[2];
+  const fecthRecomendation = async () => {
+    if (pathname.includes('meals')) {
+      const responseDetails = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      const dataDetails = await responseDetails.json();
+      setRecomendation(dataDetails.drinks);
+    } else {
+      const responseDetails = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      const dataDetails = await responseDetails.json();
+      setRecomendation(dataDetails.meals);
+    }
+  };
 
-//   const fetchRecipe = async () => {
-//     if (pathname.includes('/meals')) {
-//       const responseDetails = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-//       const dataDetails = await responseDetails.json().meals;
-//       setDetails(dataDetails);
-//     }
-//   };
+  const fetchRecipe = async () => {
+    const id = pathname.split('/')[2];
+    if (pathname.includes('meals')) {
+      const responseDetails = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const dataDetails = await responseDetails.json();
+      const infoDetails = dataDetails.meals[0];
 
-//   return (
-//     <h1>{ id }</h1>
-//   );
-// }
+      const ingredientsObject = Object.fromEntries(Object.entries(infoDetails)
+        .filter(([key]) => key.includes('strIngredient')));
+      const ingredientsList = Object.values(ingredientsObject);
 
-// export default RecipeDetails;
+      const measuresObject = Object.fromEntries(Object.entries(infoDetails)
+        .filter(([key]) => key.includes('strMeasure')));
+      const measuresList = Object.values(measuresObject);
+
+      const ingredientsNMeasures = ingredientsList.map((item, index) => ({
+        ingredient: item,
+        measure: measuresList[index],
+      }));
+
+      const youtube = details.strYoutube
+        ? details.strYoutube.replace('watch?v=', 'embed/') : undefined;
+
+      setDetails(infoDetails);
+      setIngredients(ingredientsNMeasures);
+      setYoutubeVideo(youtube);
+      setCategoryOrAlcoholic(infoDetails.strCategory);
+    } else {
+      const responseDetails = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const dataDetails = await responseDetails.json();
+      const infoDetails = dataDetails.drinks[0];
+
+      const ingredientsObject = Object.fromEntries(Object.entries(infoDetails)
+        .filter(([key]) => key.includes('strIngredient')));
+      const ingredientsList = Object.values(ingredientsObject);
+
+      const measuresObject = Object.fromEntries(Object.entries(infoDetails)
+        .filter(([key]) => key.includes('strMeasure')));
+      const measuresList = Object.values(measuresObject);
+
+      const ingredientsNMeasures = ingredientsList.map((item, index) => ({
+        ingredient: item,
+        measure: measuresList[index],
+      }));
+
+      setDetails(infoDetails);
+      setIngredients(ingredientsNMeasures);
+      setCategoryOrAlcoholic(infoDetails.strAlcoholic);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecipe();
+    fecthRecomendation();
+  }, []);
+
+  console.log(recomendation);
+
+  return (
+    <>
+      { pathname.includes('meals')
+        && (
+          <div>
+            <h1 data-testid="recipe-title">{ details.strMeal }</h1>
+            <img
+              data-testid="recipe-photo"
+              src={ details.strMealThumb }
+              alt={ details.strMeal }
+            />
+            <p data-testid="recipe-category">{ categoryOrAlcoholic }</p>
+            <p data-testid="instructions">{ details.strInstructions }</p>
+          </div>
+        ) }
+      { pathname.includes('drinks')
+        && (
+          <div>
+            <h1 data-testid="recipe-title">{ details.strDrink }</h1>
+            <img
+              data-testid="recipe-photo"
+              src={ details.strDrinkThumb }
+              alt={ details.strDrink }
+            />
+            <p data-testid="recipe-category">{ categoryOrAlcoholic }</p>
+            <p data-testid="instructions">{ details.strInstructions }</p>
+          </div>
+        ) }
+      <iframe
+        src={ youtubeVideo }
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+        title="video"
+        data-testid="video"
+      />
+      {
+        ingredients.map((item, index) => (
+          <p
+            key={ index }
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { item.ingredient }
+            { item.measure }
+          </p>
+        ))
+      }
+    </>
+  );
+}
+
+export default RecipeDetails;
